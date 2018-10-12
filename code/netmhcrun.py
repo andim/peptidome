1913e35f601f3f8435ebcc09994d97e683dd0e7b
Andreas Mayer <andisspam@gmail.com>
2018-10-11 20:40:45 -0400

    remove space from filename

--- code/netmhcrun.py
+++ code/netmhcrun.py
@@ -9,16 +9,21 @@ dfproteomes = pd.read_csv('../data/proteomes.csv', sep=',')
 dfhla = pd.read_csv('../data/hlas.csv', sep='\t', skiprows=1)
 
 for idx, row in dfproteomes.iterrows():
+    if idx < 2:
+        continue
     fastapath = datadir + row['path']
     name = row['shortname']
 
-    outname = datadir + 'netmhc/%s' % name
+    outname = datadir + 'netmhc/%s' % name.replace(' ', '')
 
     for hla in dfhla['name']:
         fullout = '%s-%s.csv' % (outname, hla)
         if not os.path.exists(fullout):
             #netMHC fasta -xls -xlsfile out.csv -a HLA-A0101
-            subprocess.run(['netMHC', fastapath, '-xls', '-xlsfile %s' %fullout, '-a %s'%hla])
+            subprocess.run(['netMHC', fastapath,
+                            '-xls',
+                            '-xlsfile %s' % fullout,
+                            '-a %s'%hla])
             df = pd.read_csv(fullout, sep='\t', skiprows=1)
             dfbinders = df[df['nM']<500]
             dfbinders.to_csv(fullout)
