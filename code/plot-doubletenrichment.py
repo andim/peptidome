da7550a3868b07709ee676fe4eabf07d92bce7cb
Andreas Mayer <andisspam@gmail.com>
2018-10-23 00:58:08 -0400

    started writing up results

--- code/plot-doubletenrichment.py
+++ code/plot-doubletenrichment.py
@@ -16,12 +16,13 @@ entropy1 = entropy(df1['freq'], base=2)
 
 meanabsfoldchanges = []
 mutualinformations = []
-gaps = np.arange(0, 201, 1)
+gaps = np.arange(0, 5, 1)
 for gap in gaps:
     df2 = counter_to_df(count_kmers_proteome(proteome, k=2, gap=gap))
     df2 = df2[~df2['seq'].str.contains('U|B|X|Z')]
     entropy2 = entropy(df2['freq'], base=2)
     mi = 2*entropy1 - entropy2
+    print(gap, mi)
     mutualinformations.append(mi)
     strcolumn_to_charcolumns(df2, 'seq')
     df2['theory'] = [float(df1.loc[s[0]] * df1.loc[s[1]]) for s in df2['seq']]
@@ -29,11 +30,13 @@ for gap in gaps:
     print(np.abs(df2['fold']).max(), np.abs(df2['fold']).mean(), mi)
     meanabsfoldchanges.append(np.abs(df2['fold']).mean())
     dfmat = df2.pivot(columns='aa1', index='aa2')['fold']
-    fig = plt.figure(figsize=(5.5, 5))
-    sns.heatmap(dfmat, vmin=-1.0, vmax=1.0, cmap='RdBu_r', cbar_kws=dict(label='log$_2$ doublet fold enrichment'))
-    fig.tight_layout()
-    fig.savefig('plots/doublet-%s-gap%i.png'%(name, gap), dpi=300)
-    plt.close(fig)
+    print(dfmat)
+    dfmat.to_csv('doubletfoldenrichment-%g-%s.csv'%(gap, name))
+#    fig = plt.figure(figsize=(5.5, 5))
+#    sns.heatmap(dfmat, vmin=-1.0, vmax=1.0, cmap='RdBu_r', cbar_kws=dict(label='log$_2$ doublet fold enrichment'))
+#    fig.tight_layout()
+#    fig.savefig('plots/doublet-%s-gap%i.png'%(name, gap), dpi=300)
+#    plt.close(fig)
 
 #df2 = counter_to_df(count_kmers_iterable(scrambled(fasta_iter(human, returnheader=False)), k=2, gap=0))
 #df2 = df2[~df2['seq'].str.contains('U|B|X|Z')]
@@ -41,13 +44,13 @@ for gap in gaps:
 #mishuffled = 2*entropy1-entropy(df2['freq'], base=2)
 #print(mishuffled)
 
-fig = plt.figure(figsize=(5, 3))
-plt.plot(gaps, meanabsfoldchanges)
-plt.ylim(0.0)
-plt.xlabel('Gap')
-plt.ylabel('Mean absolute fold change')
-fig.tight_layout()
-fig.savefig('plots/doublet-%s-meangap.png'%name, dpi=300)
+#fig = plt.figure(figsize=(5, 3))
+#plt.plot(gaps, meanabsfoldchanges)
+#plt.ylim(0.0)
+#plt.xlabel('Gap')
+#plt.ylabel('Mean absolute fold change')
+#fig.tight_layout()
+#fig.savefig('plots/doublet-%s-meangap.png'%name, dpi=300)
 
 df = pd.DataFrame.from_dict(dict(gaps=gaps, mutualinformation=mutualinformations))
 df.to_csv('mutualinformation-%s.csv'%name)
