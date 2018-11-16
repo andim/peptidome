486900a652d2bdf0dd173449fbc2ea58e83f37b8
Andreas Mayer <andisspam@gmail.com>
2018-11-16 13:36:03 -0500

    added run file to mivsdist dir

--- code/mivsdist/plot-mutualinformation.py
+++ code/mivsdist/plot-mutualinformation.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('..')
 import numpy as np
 import pandas as pd
 from scipy.stats import entropy
@@ -6,7 +8,7 @@ import matplotlib.pyplot as plt
 from lib import *
 mishuffled = 0.0012531812133396159
 
-df = pd.read_csv('mutualinformation-human.csv')
+df = pd.read_csv('data/mutualinformation-human.csv')
 fig = plt.figure(figsize=(5, 3))
 plt.plot(df['gaps']+1, df['mutualinformation'], label='data')
 plt.axhline(mishuffled, color='k', label='shuffled within proteins')
@@ -17,6 +19,6 @@ plt.xscale('log')
 plt.xlabel('Distance')
 plt.ylabel('Mutual information in bits')
 fig.tight_layout()
-fig.savefig('plots/doublet-mutualinformation.png', dpi=300)
-fig.savefig('../paper/images/mutualinformationdecay.pdf')
+fig.savefig('../plots/doublet-mutualinformation.png', dpi=300)
+fig.savefig('../../paper/images/mutualinformationdecay.pdf')
 plt.show()
---
+++ code/mivsdist/run.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.append('..')
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from lib import *
+
+name = 'human'
+proteome = human
+
+entropyestimator = entropy_grassberger
+
+df1 = counter_to_df(count_kmers_proteome(proteome, k=1))
+df1 = df1[~df1['seq'].str.contains('U|B|X|Z')]
+df1.set_index('seq', inplace=True)
+entropy1 = entropyestimator(df1['freq'], base=2)
+
+meanabsfoldchanges = []
+mutualinformations = []
+gaps = np.arange(0, 201, 1)
+for gap in gaps:
+    df2 = counter_to_df(count_kmers_proteome(proteome, k=2, gap=gap), norm=False)
+    df2 = df2[~df2['seq'].str.contains('U|B|X|Z')]
+    entropy2 = entropyestimator(df2['count'], base=2)
+    df = strcolumn_to_charcolumns(df2, 'seq')
+    e1 = entropyestimator(df.groupby('aa1').agg(np.sum)['count'], base=2)
+    e2 = entropyestimator(df.groupby('aa2').agg(np.sum)['count'], base=2)
+    mi = e1 + e2 - entropy2
+    mutualinformations.append(mi)
+
+df = pd.DataFrame.from_dict(dict(gaps=gaps, mutualinformation=mutualinformations))
+df.to_csv('data/mutualinformation-%s.csv'%name)
