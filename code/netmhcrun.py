6d1c5615753ccd6de858fb160c32beed6c0f409c
q.marcou <quentin.marcou@ens.fr>
2019-05-21 21:40:48 +0200

    Fix some netMHC call path issues and make process silent

--- code/netmhcrun.py
+++ code/netmhcrun.py
@@ -5,13 +5,15 @@ import pandas as pd
 
 from lib import *
 
-dfproteomes = pd.read_csv('../data/proteomes.csv', sep=',')
-dfhla = pd.read_csv('../data/hlas.csv', sep='\t', skiprows=1)
+FNULL = open(os.devnull, 'w') # Mute the subprocess (prevents shell cluttering)
+
+dfproteomes = pd.read_csv(datadir+'proteomes.csv', sep=',')
+dfhla = pd.read_csv(datadir+'hlas.csv', sep='\t', skiprows=1)
 
 for idx, row in dfproteomes.iterrows():
     if idx < 2:
         continue
-    fastapath = datadir + row['path']
+    fastapath = datadir + row['proteomeid'] + row['shortname']
     name = row['shortname']
 
     outname = datadir + 'netmhc/%s' % name.replace(' ', '')
@@ -20,10 +22,13 @@ for idx, row in dfproteomes.iterrows():
         fullout = '%s-%s.csv' % (outname, hla)
         if not os.path.exists(fullout):
             #netMHC fasta -xls -xlsfile out.csv -a HLA-A0101
-            subprocess.run(['netMHC', fastapath,
+            subprocess.run(['netMHC', '-f', fastapath+".fasta",
                             '-xls',
                             '-xlsfile %s' % fullout,
-                            '-a %s'%hla])
+                            '-a %s'%hla],
+                            stdout=FNULL, stderr=subprocess.STDOUT); # Mute the 
+                            #subprocess (prevents shell cluttering) 
+
             df = pd.read_csv(fullout, sep='\t', skiprows=1)
             dfbinders = df[df['nM']<500]
             dfbinders.to_csv(fullout)
