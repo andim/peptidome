ae212402e65975376c8c9dcc7ae1c45f4ffca719
Andreas Mayer <andisspam@gmail.com>
2019-09-11 21:52:27 -0400

    added shuffled mutualinformation

--- code/mivsdist/plot.py
+++ code/mivsdist/plot.py
@@ -7,22 +7,20 @@ import sys
 sys.path.append('..')
 from lib import *
 
-fig, ax = plt.subplots()
 for species in ['Human', 'Mouse', 'Yeast']:
+    fig, ax = plt.subplots()
     df = pd.read_csv('data/mutualinformation-%s.csv'%species)
-    ax.plot(df['gaps']+1, df['mutualinformation'], label=species)
-
-#df = pd.read_csv('../pfam/data/mutualinformation.csv')
-#ax.plot(df['gaps']+1, df['mutualinformation'], label='human filtered')
-#mishuffled = 0.0012531812133396159
-#plt.axhline(mishuffled, color='k', label='human shuffled')
-ax.legend()
-ax.set_ylim(0.0)
-ax.set_xlim(1.0, 200.0)
-ax.set_xscale('log')
-ax.set_xlabel('Distance')
-ax.set_ylabel('Mutual information in bits')
-fig.tight_layout()
-fig.savefig('mivsdist.pdf')
-fig.savefig('main.png')
-plt.show()
+    ax.plot(df['gaps']+1, df['mutualinformation'], label='data')
+    ax.plot(df['gaps']+1, df['shuffledmutualinformation'], label='shuffled')
+    ax.legend()
+    ax.set_ylim(0.0, 0.0145)
+    ax.set_xlim(1.0, 200.0)
+    ax.set_xscale('log')
+    ax.set_xlabel('Distance')
+    ax.set_ylabel('Mutual information in bits')
+    fig.tight_layout()
+    if species == 'Human':
+        fig.savefig('main.png')
+        plt.show()
+    else:
+        fig.savefig('%s.png'%species)
--- code/mivsdist/run.py
+++ code/mivsdist/run.py
@@ -14,17 +14,29 @@ print(name, proteome)
 
 entropyestimator = lambda x: entropy_grassberger(x, base=2)
 
-mutualinformation = []
-gaps = np.arange(0, 201, 1)
-for gap in gaps:
-    df2 = Counter(proteome, k=2, gap=gap).to_df(norm=False, clean=True)
+def calc_mi(df2):
     entropy2 = entropyestimator(df2['count'])
     df = strcolumn_to_charcolumns(df2, 'seq')
     e1 = entropyestimator(df.groupby('aa1').agg(np.sum)['count'])
     e2 = entropyestimator(df.groupby('aa2').agg(np.sum)['count'])
     mi = e1 + e2 - entropy2
-    print(gap, mi)
+    return mi
+
+mutualinformation = []
+shuffled_mutualinformation = []
+gaps = np.arange(0, 201, 1)
+for gap in gaps:
+    df2 = Counter(proteome, k=2, gap=gap).to_df(norm=False, clean=True)
+    mi = calc_mi(df2)
     mutualinformation.append(mi)
 
-df = pd.DataFrame.from_dict(dict(gaps=gaps, mutualinformation=mutualinformation))
+    # calculate shuffled mi
+    iterable = scrambled(fasta_iter(proteome, returnheader=False))
+    df2 = Counter(iterable, k=2, gap=gap).to_df(norm=False, clean=True)
+    shuffledmi = calc_mi(df2)
+    shuffled_mutualinformation.append(shuffledmi)
+    print(gap, mi, shuffledmi)
+
+df = pd.DataFrame.from_dict(dict(gaps=gaps, mutualinformation=mutualinformation,
+                                 shuffledmutualinformation=shuffled_mutualinformation))
 df.to_csv('data/mutualinformation-%s.csv'%name)
