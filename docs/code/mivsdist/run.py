6a5f559f5c1964e909d18f3518648a72df855d09
Andreas Mayer <andisspam@gmail.com>
2019-09-19 00:34:05 -0400

    updated to latest version

--- docs/code/mivsdist/run.py
+++ docs/code/mivsdist/run.py
@@ -12,19 +12,33 @@ else:
     proteome = proteome_path(name)
 print(name, proteome)
 
-entropyestimator = lambda x: entropy_grassberger(x, base=2)
+def calc_mi_std(seqs, gap):
+    mis = []
+    for i in range(30):
+        df2 = Counter(random.sample(seqs, 10000), k=2, gap=gap).to_df(norm=False, clean=True)
+        mis.append(calc_mi(df2))
+    return np.std(mis, ddof=1)/2**.5
 
 mutualinformation = []
+mutualinformation_std = []
+shuffled_mutualinformation = []
 gaps = np.arange(0, 201, 1)
 for gap in gaps:
-    df2 = Counter(proteome, k=2, gap=gap).to_df(norm=False, clean=True)
-    entropy2 = entropyestimator(df2['count'])
-    df = strcolumn_to_charcolumns(df2, 'seq')
-    e1 = entropyestimator(df.groupby('aa1').agg(np.sum)['count'])
-    e2 = entropyestimator(df.groupby('aa2').agg(np.sum)['count'])
-    mi = e1 + e2 - entropy2
-    print(gap, mi)
+    seqs = [s for s in fasta_iter(human, returnheader=False)]
+    df2 = Counter(seqs, k=2, gap=gap).to_df(norm=False, clean=True)
+    mi = calc_mi(df2)
     mutualinformation.append(mi)
+    mi_std = calc_mi_std(seqs, gap)
+    mutualinformation_std.append(mi_std)
 
-df = pd.DataFrame.from_dict(dict(gaps=gaps, mutualinformation=mutualinformation))
+    # calculate shuffled mi
+    iterable = scrambled(fasta_iter(proteome, returnheader=False))
+    df2 = Counter(iterable, k=2, gap=gap).to_df(norm=False, clean=True)
+    shuffledmi = calc_mi(df2)
+    shuffled_mutualinformation.append(shuffledmi)
+    print(gap, mi, mi_std, shuffledmi)
+
+df = pd.DataFrame.from_dict(dict(gaps=gaps, mutualinformation=mutualinformation,
+                                 mutualinformation_std=mutualinformation_std,
+                                 shuffledmutualinformation=shuffled_mutualinformation))
 df.to_csv('data/mutualinformation-%s.csv'%name)
