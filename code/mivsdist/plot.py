6e3aceb7ac2bcaac80fa7013899e6b92a6e843ff
Andreas Mayer <andisspam@gmail.com>
2019-10-07 22:26:32 +0200

    mivsdist plot with errorbars

--- code/mivsdist/plot.py
+++ code/mivsdist/plot.py
@@ -7,14 +7,15 @@ import sys
 sys.path.append('..')
 from lib import *
 
-for species in ['Human', 'Mouse', 'Yeast']:
+for species in ['Human', 'Mouse', 'Yeast', 'Viruses']:
     fig, ax = plt.subplots()
     df = pd.read_csv('data/mutualinformation-%s.csv'%species)
-    ax.plot(df['gaps']+1, df['mutualinformation'], label='data')
+    ax.errorbar(df['gaps']+1, df['mutualinformation'],
+            2*df['mutualinformation_std'], label='data')
     ax.plot(df['gaps']+1, df['shuffledmutualinformation'], label='shuffled')
     ax.legend()
     ax.set_ylim(0.0, 0.0145)
-    ax.set_xlim(1.0, 200.0)
+    ax.set_xlim(0.95, 201.0)
     ax.set_xscale('log')
     ax.set_xlabel('Distance')
     ax.set_ylabel('Mutual information in bits')
