46565fada12fa6214400e08004bc3d7fd8916c75
Andreas Mayer <andisspam@gmail.com>
2019-10-10 20:11:41 -0400

    Nicer mivsdist plots

--- code/mivsdist/plot.py
+++ code/mivsdist/plot.py
@@ -10,11 +10,16 @@ from lib import *
 for species in ['Human', 'Mouse', 'Yeast', 'Viruses']:
     fig, ax = plt.subplots()
     df = pd.read_csv('data/mutualinformation-%s.csv'%species)
-    ax.errorbar(df['gaps']+1, df['mutualinformation'],
-            2*df['mutualinformation_std'], label='data')
-    ax.plot(df['gaps']+1, df['shuffledmutualinformation'], label='shuffled')
+    l, = ax.plot(df['gaps']+1, df['mutualinformation'], lw=.8)
+    ax.fill_between(df['gaps']+1,
+                    df['mutualinformation']-df['mutualinformation_std'],
+                    df['mutualinformation']+df['mutualinformation_std'],
+                    color=l.get_color(), alpha=.3)
+    #ax.errorbar(df['gaps']+1, df['mutualinformation'],
+    #        2*df['mutualinformation_std'], label='data')
+    ax.plot(df['gaps']+1, df['shuffledmutualinformation'], label='shuffled', lw=.8)
     ax.legend()
-    ax.set_ylim(0.0, 0.0145)
+    ax.set_ylim(0.0, 0.0185)
     ax.set_xlim(0.95, 201.0)
     ax.set_xscale('log')
     ax.set_xlabel('Distance')
--- code/mivsdist/plot_loglog.py
+++ code/mivsdist/plot_loglog.py
@@ -7,22 +7,24 @@ import sys
 sys.path.append('..')
 from lib import *
 
-species = 'Human'
 fig, ax = plt.subplots()
-df = pd.read_csv('data/mutualinformation-%s.csv'%species)
-ax.errorbar(df['gaps']+1, df['mutualinformation'],
-        2*df['mutualinformation_std'], label='data')
-ax.plot(df['gaps']+1, df['shuffledmutualinformation'], label='shuffled')
+for species in ['Human', 'Yeast']:
+    df = pd.read_csv('data/mutualinformation-%s.csv'%species)
+    err = ax.errorbar(df['gaps']+1, df['mutualinformation'],
+            2*df['mutualinformation_std'], label='data '+species)
+    ax.plot(df['gaps']+1, df['shuffledmutualinformation'],
+            label='shuffled '+species, c=err.lines[0].get_color())
 d = np.linspace(1, 200)
-ax.plot(d, 0.005*np.exp(-d/50), zorder=10, label='exponential ($d_c=50$)')
-ax.plot(d, 0.025*d**-.5, zorder=10, label=r'power law ($\alpha -1/2$)')
-ax.legend(loc='upper right')
-ax.set_ylim(0.001, 0.05)
+#ax.plot(d, 0.005*np.exp(-d/50), zorder=10, label='exponential ($d_c=50$)')
+l, = ax.plot(d, 0.02*d**-.5, zorder=10, c='k', label=r'power law ($-1/2$)')
+ax.plot(d, 0.009*d**-.5, zorder=10, c=l.get_color())
+ax.legend(loc='upper right', ncol=2)
+ax.set_ylim(0.0004, 0.05)
 ax.set_xlim(0.95, 201.0)
 ax.set_xscale('log')
 ax.set_yscale('log')
 ax.set_xlabel('Distance')
 ax.set_ylabel('Mutual information in bits')
 fig.tight_layout()
-fig.savefig('Human_loglog.png')
+fig.savefig('loglog.png')
 plt.show()
