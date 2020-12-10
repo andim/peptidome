bca4dac2553e1893f86c268133e9bb2b3851c45b
Andreas Mayer <andisspam@gmail.com>
2020-12-09 19:34:52 -0500

    evaluate energies script

---
+++ code/maxent/evaluate_energies.py
@@ -0,0 +1,24 @@
+import os.path
+import numpy as np
+import pandas as pd
+
+import sys
+sys.path.append('..')
+from lib import *
+from lib.maxent import *
+
+matrix = load_matrix(snakemake.input[0])
+
+params = np.load(snakemake.input[1])
+h = params['h']
+J = params['J']
+J2 = params['J2']
+hi = params['hi']
+Jij = params['Jij']
+@njit
+def energy(x):
+    return energy_nskewfcov(x, h=h, J=J, J2=J2, hi=hi, Jij=Jij)
+
+energies = np.array([energy(x) for x in matrix])
+
+np.savez_compressed(snakemake.output[0], energies=energies)
