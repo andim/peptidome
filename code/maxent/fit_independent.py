c071520c426ab704f9f356425a568d9ab3dc3561
Andreas Mayer <andisspam@gmail.com>
2020-12-09 21:12:54 -0500

    independent model snakemake

--- code/maxent/fit_independent.py
+++ code/maxent/fit_independent.py
@@ -6,17 +6,17 @@ import sys
 sys.path.append('..')
 from lib import *
 
-from numba import njit
 
-L = 9
+k = int(snakemake.wildcards.k)
 pseudocount = 1.0
 
-matrix = load_matrix('data/train_matrix_L%i.csv.gz'%L)
+matrix = load_matrix(snakemake.input[0])
 
-flat = np.random.choice(np.arange(0, len(aminoacids), 1), size=matrix.shape)
-np.savetxt('data/flat_matrix_L%i.csv.gz'%L, flat, fmt='%i')
+#flat = np.random.choice(np.arange(0, len(aminoacids), 1), size=matrix.shape)
+#np.savetxt('data/flat_matrix_L%i.csv.gz'%L, flat, fmt='%i')
 
 fi = frequencies(matrix, num_symbols=len(aminoacids), pseudocount=pseudocount)
 f = fi.mean(axis=0)
-independent = np.random.choice(np.arange(0, 20, 1), size=matrix.shape, p=f)
-np.savetxt('data/independent_matrix_L%i.csv.gz'%L, independent, fmt='%i')
+model_matrix = np.random.choice(np.arange(0, 20, 1), size=matrix.shape, p=f)
+np.savetxt(snakemake.output[0], model_matrix, fmt='%i')
+np.savez(snakemake.output[1], f=f)
