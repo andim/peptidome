586bd8ad05dcbb66c07285050b57581fcbe4a012
Andreas Mayer <andisspam@gmail.com>
2020-12-10 18:21:52 -0500

    entropy calculation snakemake

--- code/maxent/common.py
+++ code/maxent/common.py
@@ -1,13 +1,19 @@
 model_hierarchy = ['model_ncov', 'model', 'model_nskewdiag', 'model_nskew', 'model_nskewfcov']
 
-labels = {'test': 'test set',
+labels = {
           'train': 'training set',
           'model': '2-point',
           'model_ncov':'2nd moment',
-          'independent' : '1-point',
           'model_nskew': '3rd moment',
           'model_nskewdiag': '3rd moment diag',
-          'model_nskewfcov' : '3rd moment, 2-point'}
+          'model_nskewfcov' : '3rd moment, 2-point',
+          'uniform' : 'uniform',
+          'test': 'test set',
+          'independent': '1st moment',
+          'ncov' : '2nd moment',
+          'nskew' : '3rd moment',
+          'nskewfcov' : '2-point'
+          }
 
 def set_aminoacidslabel(ax, aminoacidorder):
     naminoacids = len(aminoacidorder)
