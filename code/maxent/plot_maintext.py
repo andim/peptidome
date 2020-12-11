16b175da5efc08bca90be1e203fb9d57baff47ea
Andreas Mayer <andisspam@gmail.com>
2020-12-10 19:22:02 -0500

    maintext maxent figure fix

--- code/maxent/plot_maintext.py
+++ code/maxent/plot_maintext.py
@@ -8,6 +8,7 @@ import sys
 sys.path.append('..')
 from lib import *
 plt.style.use('../peptidome.mplstyle')
+from common import labels
 
 k = 9
 fig, axes = plt.subplots(figsize=(7.2, 5.0), ncols=3, nrows=2)
@@ -46,13 +47,6 @@ for ax in axes[0, 1:]:
     ax.ticklabel_format(style='sci', scilimits=(0,0))
 
 ## dos
-labels = {
-          'test': 'test set',
-          'independent': '1st moment',
-          'ncov' : '2nd moment',
-          'nskew' : '3rd moment',
-          'nskewfcov' : '3rd moment, 2-point'
-          }
 energies = {}
 models = ['test', 'independent', 'ncov', 'nskew', 'nskewfcov']
 for model in models:
@@ -72,6 +66,36 @@ axes[1, 0].legend(loc='upper left')
 axes[1, 1].get_legend().remove()
 axes[1, 1].set_yscale('log')
 
+
+ax = axes[1, 2]
+models = ['uniform', 'independent', 'ncov', 'nskew', 'nskewfcov']
+entropies = np.zeros(len(models))
+for i, model in enumerate(models):
+    if model == 'uniform':
+        S = k*np.log(20)
+    else:
+        S = np.float(pd.read_csv('data/Human_{model}_k{k}_entropy.csv'.format(model=model, k=k),
+                        header=None, index_col=0).loc['S'])
+    entropies[i] = S
+data = np.exp(entropies)
+
+labels = [labels[model] for model in models[1:]]
+width = 0.5  # the width of the bars
+x = np.arange(len(labels))  # the label locations
+rects = ax.bar(x, data[1:]/data[0], width)
+ax.set_xticks(x)
+ax.set_xticklabels(labels, rotation=90)
+#ax.axhline(data[1]/data[0], ls='--')
+
+reduction = np.abs((data[2:]-data[1])/data[1])
+for rect, toplabel in zip(rects[1:], reduction):
+    ax.annotate('-{0:.0f}%'.format(100*toplabel),
+                xy=(rect.get_x()+width/2, rect.get_height()),
+                xytext=(0, 1),  # 3 points vertical offset
+                textcoords="offset points",
+                ha='center', va='bottom', fontsize='xx-small')
+
+ax.set_ylabel('Effective Diversity\n$\exp(S)/20^k$')
 
 label_axes(fig, labelstyle='%s', xy=(-0.25, 1.0))
 fig.tight_layout()
