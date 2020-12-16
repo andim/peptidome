be86a46d948566c9a3831b4f41824358fe74c152
Andreas Mayer <andisspam@gmail.com>
2020-12-16 00:47:45 -0500

    humanviruses maxent added

--- code/maxent/generate_reference_matrices.py
+++ code/maxent/generate_reference_matrices.py
@@ -18,23 +18,18 @@ prng = np.random.RandomState(seed=seed)
 if proteome == 'Humannozf':
     seqs = np.array(pd.read_csv('../pfam/data/human_nozf.csv')['Sequence'])
 else:
-    df = load_proteome_as_df(proteome)
+    if proteome == 'Humanviruses':
+        df = load_unirefproteome_as_df_path(datadir+'human-viruses-uniref90-filtered.fasta')
+    else:
+        df = load_proteome_as_df(proteome)
     df.drop_duplicates(subset=['Sequence'], inplace=True)
     seqs = df['Sequence']
 
 train, test = train_test_split(seqs, test_size=0.5, random_state=prng)
 
-
-def filterseqs(seqs, k, filterlength):
-    counter = count_kmers_iterable(seqs, filterlength, clean=True)
-    count_df = pd.DataFrame.from_dict(counter, orient='index', columns=['count'])
-    count_series = count_df.T.squeeze()
-    filtered = np.asarray(count_series.index.str[:k])
-    return filtered
-
 for i, (label, data) in enumerate([('train', train), ('test', test)]):
     if proteome == 'Humannozf':
         matrix = kmers_to_matrix(to_kmers(data, k=k))
     else:
-        matrix = kmers_to_matrix(filterseqs(data, k=k, filterlength=filterlength))
+        matrix = kmers_to_matrix(filter_unique(data, k=k, filterlength=filterlength))
     np.savetxt(snakemake.output[i], matrix, fmt='%i')
