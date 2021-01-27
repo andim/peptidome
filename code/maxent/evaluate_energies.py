c061fb691d2a03982496a4fe133115c209945c9a
Andreas Mayer <andisspam@gmail.com>
2021-01-27 18:15:07 -0500

    energies as likelihoods

--- code/maxent/evaluate_energies.py
+++ code/maxent/evaluate_energies.py
@@ -19,6 +19,8 @@ Jij = params['Jij']
 def energy(x):
     return energy_nskewfcov(x, h=h, J=J, J2=J2, hi=hi, Jij=Jij)
 
+F = np.float(pd.read_csv(snakemake.input[2], header=None, index_col=0).loc['F'])
+
 energies = np.array([energy(x) for x in matrix])
 
-np.savez_compressed(snakemake.output[0], energies=energies)
+np.savez_compressed(snakemake.output[0], energies=energies-F)
