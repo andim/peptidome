77d5df4078c28bea018dd4f8967f20489d4ab7f3
Andreas Mayer <andisspam@gmail.com>
2022-05-04 18:37:37 +0100

    figures updated

--- code/theory_sketches/shell.py
+++ code/theory_sketches/shell.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('..')
+from lib import *
 import numpy as np
 import pandas as pd
 import scipy.stats
@@ -5,19 +8,20 @@ import matplotlib.pyplot as plt
 
 plt.style.use('../peptidome.mplstyle')
 
-fig, ax = plt.subplots(figsize=(2.0, 1.5))
+fig, ax = plt.subplots(figsize=(2.25, 1.75))
 x = np.linspace(0, 1)
 
 
 def shell(distance):
     return 1.0/(1+np.exp(-(distance-0.05)/0.05))**5 * 1/(1+np.exp((distance-0.2)/0.5))
 
-ax.plot(x, shell(x)/max(shell(x)))
-ax.set_xlabel('Distance to self')
+ax.plot(x, shell(x)/max(shell(x)), label='Shell Model')
+ax.legend(title='Theory Expectation', loc='lower right')
+ax.set_xlabel('Distance to Self')
 ax.set_xticks([0, 1])
 ax.set_yticks([0, 1])
 ax.set_ylabel('Immunogenicity')
 fig.tight_layout()
 plt.show()
 fig.savefig('shell.png')
-fig.savefig('../../figures/raw/theory_sketch_shell.svg')
+fig.savefig(figuredir+'shell.svg')
