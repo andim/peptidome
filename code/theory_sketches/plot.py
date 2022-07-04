768fe457bb1ed6696a2263117dd554659b608a67
Andreas Mayer <andisspam@gmail.com>
2022-07-04 18:37:19 +0200

    axes labels for theory curves

--- code/theory_sketches/plot.py
+++ code/theory_sketches/plot.py
@@ -8,12 +8,12 @@ import matplotlib.pyplot as plt
 
 plt.style.use('../peptidome.mplstyle')
 
-fig, ax = plt.subplots(figsize=(2.25, 1.75))
+fig, axes = plt.subplots(figsize=(2*2.1, 1.6), ncols=2)
 x = np.linspace(0, 1)
 
 
 def hole(distance):
-    if distance < 0.35:
+    if distance < 0.45:
         return 0.0
     else:
         return 1.0
@@ -21,13 +21,19 @@ def hole(distance):
 def outlier(distance):
     return 1/(1+np.exp(-(distance-0.25)/0.15))**3
 
-ax.plot(x, np.vectorize(hole)(x), label='Strong Whitelisting')
-ax.plot(x, np.vectorize(outlier)(x), label='Outlier Detection')
-ax.legend(title='Theory Expectation', loc='lower right')
-ax.set_xlabel('Distance to Self')
-ax.set_xticks([0, 1])
-ax.set_yticks([0, 1])
-ax.set_ylabel('Immunogenicity')
+axes[0].plot(x, np.vectorize(hole)(x), label='Strong Whitelisting')
+axes[1].plot(x, np.vectorize(outlier)(x), label='Outlier Detection')
+axes[0].set_title('Whitelisting')
+axes[1].set_title('Probabilistic classification')
+for ax in axes:
+    ax.set_xlabel('Distance to Self')
+    ax.set_xticks([0, 1])
+    ax.set_xticklabels(['low', 'high'])
+    ax.xaxis.labelpad = -5
+    ax.set_yticks([0, 1])
+    ax.set_yticklabels(['low', 'high'])
+    ax.set_ylabel('Immunogenicity')
+    ax.yaxis.labelpad = -10
 fig.tight_layout()
 plt.show()
 fig.savefig('main.png')
