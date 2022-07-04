c63bbd667e2c42df07a3025066893b5209dec606
Andreas Mayer <andisspam@gmail.com>
2022-07-04 20:20:23 +0200

    updates to figures

--- code/theory_sketches/shell.py
+++ code/theory_sketches/shell.py
@@ -13,15 +13,18 @@ x = np.linspace(0, 1)
 
 
 def shell(distance):
-    return 1.0/(1+np.exp(-(distance-0.05)/0.05))**5 * 1/(1+np.exp((distance-0.2)/0.5))
+    return 1.0/(1+np.exp(-(distance-0.05)/0.05))**5 * 1/(1+np.exp((distance-0.3)/0.5))
 
 ax.plot(x, shell(x)/max(shell(x)), label='Shell Model')
 ax.legend(title='Theory Expectation', loc='lower right')
 ax.set_xlabel('Distance to Self')
 ax.set_xticks([0, 1])
+ax.set_xticklabels(['low', 'high'])
+ax.xaxis.labelpad = -5
 ax.set_yticks([0, 1])
+ax.set_yticklabels(['low', 'high'])
 ax.set_ylabel('Immunogenicity')
+ax.yaxis.labelpad = -10
 fig.tight_layout()
 plt.show()
-fig.savefig('shell.png')
-fig.savefig(figuredir+'shell.svg')
+fig.savefig(figuredir+'theory_sketch_shell.svg')
